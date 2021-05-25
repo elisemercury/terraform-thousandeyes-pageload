810ec4f97cfffb9cd8c154e4b886d766392e7dc3
elisemercury <56441652+elisemercury@users.noreply.github.com>
2021-05-25 14:36:52 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,8 +3,3 @@ variable "te_token" {
   type    = string
   description = "thousandEyes dashboard API token"
 }
-
-variable "test_page" {
-  type    = string
-  description = "website of the page that the test will be perfomed on"
-}
